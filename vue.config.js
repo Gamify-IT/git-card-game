0550d1b942f4ce8e1662f4daebfd8fc84cdfc95c
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-05-14 16:54:02 +0200

    change public path in vue config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  publicPath: './minigames/git-card-game',
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/minigames/git-card-game'
+    : '/',
   transpileDependencies: true,
 });
